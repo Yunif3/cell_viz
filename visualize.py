53c697dea30787d7715fac7ac069f76f2fae8340
Yunsoo Kim <yunsoo@umich.edu>
2021-02-06 19:05:41 -0500

    added the visualization layer that gets called by the processing layer once all the arguments are determined. Currently supports very basic line graph and passed a simple end-to-end testing

--- visualize.py
+++ visualize.py
@@ -1,3 +1,15 @@
 from matplotlib import pyplot as plt
+import numpy as np
 
-def line_plot()
+def line_plot(data, **kwargs):
+    # taking the columns
+    columns = np.array(data).T
+    print(columns)
+    x = columns[0]
+    x_label, x_data = x[0], x[1:].astype(np.float)
+    for y in columns[1:]:
+        y_label, y_data = y[0], y[1:].astype(np.float)
+        plt.plot(x_data, y_data, '-', label=y_label)
+    plt.xlabel(x_label)
+    plt.legend()
+    plt.show()
